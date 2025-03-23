d61ff82ba610e0169ba4d3682f86f5c5ea4bb97d
Sonupr130 <sonupradhan130@gmail.com>
2025-03-23 15:08:22 +0530

    UPDATE

--- Backend/server.js
+++ Backend/server.js
@@ -39,6 +39,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors({ origin: "https://ems-frontend-steel.vercel.app" }));
 
 app.use(authRoutes);
 app.use(payRoutes);
